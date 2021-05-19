a7132797d60a3994f7046f02aed6696d45c7afdf
OdehYazan <eng.yazanodeh@gmail.com>
2021-05-19 18:31:40 +0300

    Group 7

--- js/app.js
+++ js/app.js
@@ -4,19 +4,30 @@
 const Cart = function(items) {
   // this.items is an array of CartItem instances.
   this.items = items;
+  
 };
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // let cartItem = new CartItem (product,quantity);
+  new CartItem (product,quantity);
+  Cart.items.push(this)
+  
 };
+
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let data = JSON.stringify(this.items);
+  console.log(data);
+  localStorage.setItem('cart',data)
+  
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  Cart.items.splice(item,1)
 };
 
 const CartItem = function(product, quantity) {
